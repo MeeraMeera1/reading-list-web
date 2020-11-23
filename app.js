619aa334218b3c637265d9a52ef38042158e5aed
MeeraMeera1 <gatesameera98@gmail.com>
2020-11-23 00:25:07 -0500

    Now have error handler for unhandles requests and a custom error handler to log errors

--- app.js
+++ app.js
@@ -13,10 +13,22 @@ app.use(morgan("dev"));
 
 app.use(routes);
 
-app.use((req,res,next) => {
+//error handler to catch unhandled requests and forward to error handler.
+app.use((req, res, next) => {
     const error = new Error('The requested page couldn\'t be found.');
     error.status = 404;
-    next(err);
+    next(error);
 });
+
+//error handler to log errors
+app.use((error, req, res, next) => {
+    if(process.env.NODE_ENV === 'production'){
+        //todo log the error to the database
+    } else {
+        console.error(err);
+    }
+})
+
+
 
 module.exports = app;
