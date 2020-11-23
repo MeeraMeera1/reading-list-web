ff7883b36f345ca65db5a48fe086006a05b9bcca
MeeraMeera1 <gatesameera98@gmail.com>
2020-11-23 00:29:00 -0500

    Now have error handler for 404 errors

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //step 2: set up the app
 
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
-const routes = require('./routes');
+const routes = require("./routes");
 
 const app = express();
 
@@ -15,20 +15,31 @@ app.use(routes);
 
 //error handler to catch unhandled requests and forward to error handler.
 app.use((req, res, next) => {
-    const error = new Error('The requested page couldn\'t be found.');
-    error.status = 404;
-    next(error);
+  const error = new Error("The requested page couldn't be found.");
+  error.status = 404;
+  next(error);
 });
 
 //error handler to log errors
 app.use((error, req, res, next) => {
-    if(process.env.NODE_ENV === 'production'){
-        //todo log the error to the database
-    } else {
-        console.error(err);
-    }
-})
+  if (process.env.NODE_ENV === "production") {
+    //todo log the error to the database
+  } else {
+    console.error(error);
+  }
+  next(error);
+});
 
-
+//error handler for 404 errors.
+app.use((error, req, res, next) => {
+  if (error.status === 404) {
+    res.status(404);
+    res.render("page-not-found", {
+      title: "Page Not Found",
+    });
+  } else {
+    next(error);
+  }
+});
 
 module.exports = app;
